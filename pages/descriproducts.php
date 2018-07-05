a41c5816bb99ddceac85ede52bb76889c4b4ce86
Tomasl77 <lobgeois.tomas@free.fr>
2018-07-05 18:16:26 +0200

    panier presque fonctionnel

--- pages/descriproducts.php
+++ pages/descriproducts.php
@@ -15,6 +15,36 @@ $musics = $musics->fetchAll();
 foreach ($musics as $msc){
 };
 
+if(!empty($_GET["action"])) 
+{
+	switch($_GET["action"]) 
+	{
+		case "add":
+		if(!empty($_POST["quantity"])) {
+			$productByid = $DB->query("SELECT * FROM musique WHERE id_musique='" . $_GET["id"] . "'");
+			$productByid = $productByid->fetchAll();
+			$itemArray = array($productByid[0]["id_musique"]=>array('nom_album'=>$productByid[0]["nom_album"], 'id_musique'=>$productByid[0]["id_musique"], 'nom_artiste'=>$productByid[0]["nom_artiste"], 'quantity'=>$_POST["quantity"], 'prix_vinyl'=>$productByid[0]["prix_vinyl"]));
+
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByid[0]["id_musique"],array_keys($_SESSION["cart_item"]))) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+						if($productByid[0]["id_musique"] == $k) {
+							if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+								$_SESSION["cart_item"][$k]["quantity"] = 0;
+							}
+							$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+						}
+					}
+				} else {
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+				}
+			} else {
+				$_SESSION["cart_item"] = $itemArray;
+			}
+		}
+		break;
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,39 +59,41 @@ foreach ($musics as $msc){
 	<?php require_once ('../includes/bande_son.php'); ?>
 	
 	<div class="wrapper">
-	
-	<?php require_once("../includes/navbar.php"); ?>
-	
-			<div class="album">
-				<div class="titrealbum">
-					<h4>Album : <?php echo $msc['nom_album']?></h4>
-					<h5>Interprète : <?php echo $msc['nom_artiste']?></h5>
-				</div>
-				<div class="pochette">
-					<?php echo '<img src=/commerce/E/images/'.$msc['pochette'].'></img>'  ?>
-				</div>
-				<div id="productdescri">
-					<h6>Description du produit : </h6><br>
-					<?php echo $msc['description_musique']  ?>
-				</div>
-				
-				<div id="prix">
-					<div id="prixvin">
-						<p>Prix du vinyl: <br><?php echo $msc['prix_vinyl']. "€" ?> <div class="ajoutpanier"><button type="button">Ajouter au panier</button></div></p>
-					</div>
-					<div id="prixcas">
-						<p>Prix de la cassette: <br><?php echo $msc['prix_cassette']. "€" ?><div class="ajoutpanier"><button type="button">Ajouter au panier</button></div></p>
-					</div>
-					</div>
-					
+
+		<?php require_once("../includes/navbar.php"); ?>
+
+		<div class="album">
+			<div class="titrealbum">
+				<h4>Album : <?php echo $msc['nom_album']?></h4>
+				<h5>Interprète : <?php echo $msc['nom_artiste']?></h5>
 			</div>
-	<!-- <a href="./admin.php"><button type="button" name="Admin">Admin</button></a> -->
-	<?php  require_once("../includes/footer.php");?>
+			<div class="pochette">
+				<?php echo '<img src=/commerce/E/images/'.$msc['pochette'].'></img>'  ?>
+			</div>
+			<div id="productdescri">
+				<h6>Description du produit : </h6><br>
+				<?php echo $msc['description_musique']  ?>
+			</div>
+
+			<div id="prix">
+			<!-- 	<div id="prixvin">
+				<p>Prix de la cassette: <br><?php echo $msc['prix_cassette']. "€" ?> <div class="ajoutpanier"><button type="button">Ajouter au panier</button></div></p>
+			</div>
+		--><div id="prixcas">
+			<form method="post" action="descriproducts.php?action=add&id=<?php echo $idget; ?>">
+				<p>Prix du vinyl: <br><?php echo $msc['prix_vinyl']. "€" ?><div class="ajoutpanier"><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Ajouter au panier"/></div></p>
+			</form>
+		</div>
 	</div>
+
+</div>
+<!-- <a href="./admin.php"><button type="button" name="Admin">Admin</button></a> -->
+<?php  require_once("../includes/footer.php");?>
+</div>
 </body>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  <script src="/commerce/E/index.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="/commerce/E/index.js"></script>
 </html>
--- pages/panier.php
+++ pages/panier.php
@@ -7,31 +7,87 @@ if (!isset($_SESSION['prenom']))
 	header('Location: ../index.php');
 	exit;
 }
+
+if(!empty($_GET["action"]))
+{
+	switch($_GET["action"])
+	{
+		case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+				if($_GET["id_musique"] == $k)
+					unset($_SESSION["cart_item"][$k]);				
+				if(empty($_SESSION["cart_item"]))
+					unset($_SESSION["cart_item"]);
+			}
+		}
+		break;
+		case "empty":
+		unset($_SESSION["cart_item"]);
+		break;	
+	}
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <link rel="stylesheet" href="../index.css">
-  <title>Modifprofil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" href="../index.css">
+	<title>Modifprofil</title>
 </head>
 <body>
-	  <?php require_once ('../includes/bande_son.php'); ?>
+	<?php require_once ('../includes/bande_son.php'); ?>
 
 	<div class="wrapper">
-    <?php require_once("../includes/navbar.php"); ?>
-    	<div class="basketcontainer">
-    		<div id="panier">
-			Voici mon panier
-    		</div>
+		<?php require_once("../includes/navbar.php"); ?>
+		<div class="basketcontainer">
+			<div id="panier">
+				<div class="panierentete"><span>MON PANIER</span> <span id="vider"><a id="btnEmpty" href="panier.php?action=empty">Vider le panier</a></span></div>
+				<?php
+				if(isset($_SESSION["cart_item"])){
+					$item_total = 0;
+					?>	
+					<table class="table table-dark">
+						<tbody>
+							<tr>
+								<th>Nom de l'album</th>
+								<th>Artiste</th>
+								<th>Quantité</th>
+								<th>Prix vinyl</th>
+								<th></th>
+							</tr>	
+							<?php		
+							foreach ($_SESSION["cart_item"] as $item){
+								?>
+								<tr>
+									<td><strong><?php echo $item["nom_album"]; ?></strong></td>
+									<td><?php echo $item["nom_artiste"]; ?></td>
+									<td><?php echo $item["quantity"]; ?></td>
+									<td><?php echo "$".$item["prix_vinyl"]; ?></td>
+									<td><a href="panier.php?id_musique=<?php echo $item["id_musique"]; ?>&action=remove" title="Supprimer">Retirer du caddie</a></td>
+								</tr>
+								<?php
+								$item_total += ($item["prix_vinyl"]*$item["quantity"]);
+							};
+							?>
+
+							<tr>
+								<td colspan="5" align=right><strong>Total:</strong> <?php echo "$".$item_total; ?></td>
+							</tr>
+						</tbody>
+					</table>		
+					<?php
+				};
+				?>
+			</div>
 		</div>
-    <?php require_once("../includes/footer.php");?>
+		<?php require_once("../includes/footer.php");?>
 	</div>
 </body>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  <script src="/commerce/E/index.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="/commerce/E/index.js"></script>
 </html>
